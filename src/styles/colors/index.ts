67edd22950009169467098d60a9ed15c1a82c14d
Henrique Tavares <ihenrits@gmail.com>
2020-08-28 17:04:58 -0300

    style: creating style files

---
+++ src/styles/colors/index.ts
@@ -0,0 +1,94 @@
+// Primaty Colors
+export const primary = '#534b55'; // #
+export const background = '#f7f7f9'; // #
+
+// Colors
+export const white = '#FFFFFF'; // #
+export const whiteGrey = '#bebebe';
+export const red = '#F1574D';
+export const green = '#86BD67';
+export const blue = '#025687'; // #
+export const grey = '#464555'; // #
+export const orange = '#F9A03F';
+export const yellow = '#FCAB10';
+
+// Components Colors
+export const label = '#3E3434';
+export const scroll = '#B1ADAD';
+export const error = '#C33030';
+
+// Input
+export const inputWhite = '#FFFFFF';
+
+// Select
+export const selectDefault = '#DEEBFF';
+
+// Input & Select
+export const inputSelectBorder = '#656565';
+export const inputSelectPlaceholder = '#BABABA';
+
+// Button
+export const buttonGreen = '#1EBF73';
+export const buttonRed = '#FC4B6C';
+export const buttonWhite = '#F5F5F5';
+export const buttonDisabled = '#CCCCCC';
+export const buttonTextDisabled = '#928f8f';
+
+// Modal
+export const modalShadow = '#121214e6';
+
+// Table Home
+export const tableHomeDeselectedProduct = '#ffc3c3';
+
+// Numeric Input
+export const numericInputGreen = '#86bd67';
+export const numericInputWhite = '#e8e8e8';
+
+// SaleData
+export const sendColor = '#0084C4';
+export const canceledColor = '#FF4A4A';
+export const contingencyColor = '#804000';
+export const unusableColor = '#B3B3B3';
+export const pendingColor = '#840000';
+
+// Storybook
+export const storybookRed = '#DB324D';
+export const storybookGrey = '#373B43';
+export const storybookWhite = '#FFF';
+
+export default {
+  primary,
+  background,
+  white,
+  whiteGrey,
+  red,
+  green,
+  blue,
+  grey,
+  orange,
+  yellow,
+  label,
+  scroll,
+  error,
+  inputWhite,
+  selectDefault,
+  inputSelectBorder,
+  inputSelectPlaceholder,
+  buttonGreen,
+  buttonRed,
+  buttonWhite,
+  buttonDisabled,
+  buttonTextDisabled,
+  modalShadow,
+  tableHomeDeselectedProduct,
+  numericInputGreen,
+  numericInputWhite,
+  sendColor,
+  canceledColor,
+  contingencyColor,
+  unusableColor,
+  pendingColor,
+  storybookRed,
+  storybookGrey,
+  storybookWhite,
+};
---
+++ src/styles/global.ts
@@ -0,0 +1,48 @@
+import { createGlobalStyle } from 'styled-components';
+import { shade } from 'polished';
+
+import colors from './colors';
+
+export default createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    outline: 0;
+  }
+
+  ::-webkit-scrollbar {
+    width: 12px;
+    height: 14px;
+  }
+  ::-webkit-scrollbar-track {
+    box-shadow: inset 0 0 5px ${colors.scroll};
+    border-radius: 8px;
+  }
+  ::-webkit-scrollbar-thumb {
+    background: ${colors.scroll};
+    border-radius: 8px;
+  }
+  ::-webkit-scrollbar-thumb:hover {
+    background:  ${shade(0.2, colors.scroll)};
+  }
+
+  body {
+    background: ${colors.background};
+    color: ${colors.primary};
+    -webkit-font-smoothing: antialiased;
+  }
+
+  body, input, button {
+    font-family: 'Roboto Slab', serif;
+    font-size: 62.5%;
+  }
+
+  h1, h2, h3, h4, h5, h6, strong {
+    font-weight: 700;
+  }
+
+  button {
+    cursor: pointer;
+  }
+`;
