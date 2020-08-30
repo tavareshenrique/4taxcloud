2f9d8575b97f93e07f1978450931f93f5abd9dd5
Henrique Tavares <ihenrits@gmail.com>
2020-08-29 22:27:01 -0300

    style: fix loading overlay style

--- src/components/LoadingOverlay/styles.ts
+++ src/components/LoadingOverlay/styles.ts
@@ -22,9 +22,11 @@ const walking = keyframes`
 `;
 
 export const Container = styled.div`
-  width: 100%;
-  height: 100%;
-  position: absolute;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  position: fixed;
   z-index: 1;
   top: 0;
   right: 0;
@@ -39,10 +41,6 @@ export const Content = styled.div`
   align-items: center;
   justify-content: center;
 
-  margin-top: -2.5rem;
-  margin-left: -2.5rem;
-
-  position: absolute;
   top: 50%;
   left: 50%;
 
